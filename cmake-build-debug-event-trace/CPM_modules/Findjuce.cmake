43e1d582a4a0ffb3eeb62f17a9e9fc5cd5e266ab
bristowsoundsgood <bristowsoundsgood@gmail.com>
2025-11-21 23:34:02 +0000

    Removed cmake-build-debug-event-trace

--- cmake-build-debug-event-trace/CPM_modules/Findjuce.cmake
+++
@@ -1,3 +0,0 @@
-include("/Users/joebristow/Desktop/CS/Software Engineering/Mid-Level/DSP/Projects/dsp-iron-whale/cmake-build-debug-event-trace/cmake/CPM_0.34.0.cmake")
-CPMAddPackage(NAME;juce;GIT_REPOSITORY;https://github.com/juce-framework/JUCE.git;GIT_TAG;origin/master)
-set(juce_FOUND TRUE)
